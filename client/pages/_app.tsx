0410bfa6eacf10d2bd4db36ef8dc792cdbe18895
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-04-05 22:20:55 +0400

    Add chakra ui

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ProviderWeb3 } from "@lido-sdk/web3-react";
+import { ChakraProvider, DarkMode, extendTheme, ThemeConfig } from '@chakra-ui/react'
 
-import { rpc, supportedChainIds } from "../constants/web3data";
+import { rpc, supportedChainIds } from "../constants/web3data"
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+}
+
+const theme = extendTheme({
+  config
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
+  return (<ChakraProvider theme={theme}>
     <ProviderWeb3
     defaultChainId={1}
     supportedChainIds={supportedChainIds}
@@ -13,6 +22,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   >
     <Component {...pageProps} />
   </ProviderWeb3>
+  </ChakraProvider>
   )
 }
 

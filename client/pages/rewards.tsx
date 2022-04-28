73c938899804cc195043aaeaef9d8d51958bf7e9
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-04-29 04:58:57 +0530

    disable `claim` button if no netRewards

--- client/pages/rewards.tsx
+++ client/pages/rewards.tsx
@@ -6,22 +6,26 @@ import abi from "../public/abis/straddle-abi.json";
 import { getRpcProvider } from "@lido-sdk/providers";
 import { CHAINS } from "@lido-sdk/constants";
 import { rpc } from "./staking";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function Rewards() {
   const { account } = useWeb3();
   const [claimLoading, setClaimLoading] = useState(false);
   const contractAddress = "0xf72Cabed72b3936E0F952b5E96a5d95A4Ec776DF";
+  const [netRewards, setNetRewards] = useState(0);
   const providerRpc = getRpcProvider(
     CHAINS.Rinkeby,
     `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_API_KEY}`
   );
+
+  const [fetchingData, setFetchingData] = useState(false);
 
   const [balance, setBalance] = useState();
   const [rewardsClaimed, setRewardsClaimed] = useState();
 
   useEffect(() => {
     const fetchBalance = async () => {
+      setFetchingData(true);
       if (account) {
         try {
           const { ethereum } = window;
@@ -32,13 +36,17 @@ export default function Rewards() {
             provider.getSigner()
           );
 
+          const calculateRewards = await contract.calculateRewards(account);
+          setNetRewards(calculateRewards);
           const userAccounts = await contract.userAccounts(account);
           console.log("userAccounts: ", userAccounts);
           // console.log('balance: ',balance);
           setBalance(userAccounts[0]?.toString());
           setRewardsClaimed(userAccounts[1]?.toString());
+          setFetchingData(false);
         } catch (error) {
           console.log("Error while fetching userBalance", error);
+          setFetchingData(false);
         }
       }
     };
@@ -81,21 +89,33 @@ export default function Rewards() {
       <h1>REWARDS</h1>
       <br></br>
 
-      <div id="rewards-view-container">
-        <div>
+      {fetchingData ? (
+        <Text textAlign="center" fontWeight="semibold" fontSize="xl">
+          Fetching Data
+        </Text>
+      ) : (
+        <div id="rewards-view-container">
+          <div>
+            <Flex gap={2}>
+              <b>BALANCE: </b>
+              <p>{balance} Strad</p>
+            </Flex>
+            {netRewards > 0 ? (
+              <Button disabled={claimLoading} onClick={claimRewards}>
+                {claimLoading ? "Loading..." : `Claim ${netRewards} Strad`}
+              </Button>
+            ) : (
+              <Text textAlign="left" fontWeight="semibold" fontSize="md">
+                You dont have any rewards to claim
+              </Text>
+            )}
+          </div>
           <Flex gap={2}>
-            <b>BALANCE: </b>
-            <p>{balance} Strad</p>
+            <b>PAST</b>
+            <p>{rewardsClaimed}</p>
           </Flex>
-          <Button disabled={claimLoading} onClick={claimRewards}>
-            {claimLoading ? "Loading..." : "Claim"}
-          </Button>
         </div>
-        <Flex gap={2}>
-          <b>PAST</b>
-          <p>{rewardsClaimed}</p>
-        </Flex>
-      </div>
+      )}
     </div>
   );
 }
